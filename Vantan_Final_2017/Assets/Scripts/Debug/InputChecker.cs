8fcb723c46d99ddba05037091ea501c35f72bfc9
xkyoro <min_n1202@yahoo.co.jp>
2016-10-22 12:39:14 +0900

    High High on Progress

--- Vantan_Final_2017/Assets/Scripts/Debug/InputChecker.cs
+++ Vantan_Final_2017/Assets/Scripts/Debug/InputChecker.cs
@@ -1,86 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputChecker : MonoBehaviour {
+public class InputChecker : MonoBehaviour
+{
 
-  [SerializeField]
-  private GameObject DirUp = null;
-  [SerializeField]
-  private GameObject DirLeft = null;
-  [SerializeField]
-  private GameObject DirDown = null;
-  [SerializeField]
-  private GameObject DirRight = null;
+    [SerializeField]
+    private GameObject DirUp = null;
+    [SerializeField]
+    private GameObject DirLeft = null;
+    [SerializeField]
+    private GameObject DirDown = null;
+    [SerializeField]
+    private GameObject DirRight = null;
 
-  [SerializeField]
-  private GameObject ActUp = null;
-  [SerializeField]
-  private GameObject ActLeft = null;
-  [SerializeField]
-  private GameObject ActDown = null;
-  [SerializeField]
-  private GameObject ActRight = null;
+    [SerializeField]
+    private GameObject ActUp = null;
+    [SerializeField]
+    private GameObject ActLeft = null;
+    [SerializeField]
+    private GameObject ActDown = null;
+    [SerializeField]
+    private GameObject ActRight = null;
 
-  void Update() {
-    UpdateActionInputs();
-    UpdateDirectionInputs();
-  }
-
-  private void UpdateActionInputs() {
-    if (Input.GetAxis("PS4Square") == 1.0f) {
-      ActLeft.GetComponent<Renderer>().material.color = Color.red;
-    }
-    else {
-      ActLeft.GetComponent<Renderer>().material.color = Color.white;
+    void Update()
+    {
+        UpdateActionInputs();
+        UpdateDirectionInputs();
     }
 
-    if (Input.GetAxis("PS4Cross") == 1.0f) {
-      ActDown.GetComponent<Renderer>().material.color = Color.red;
-    }
-    else {
-      ActDown.GetComponent<Renderer>().material.color = Color.white;
+    private void UpdateActionInputs()
+    {
+        if (Input.GetAxis("PS4Square") == 1.0f)
+        {
+            ActLeft.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            ActLeft.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if (Input.GetAxis("PS4Cross") == 1.0f)
+        {
+            ActDown.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            ActDown.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if (Input.GetAxis("PS4Circle") == 1.0f)
+        {
+            ActRight.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            ActRight.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if (Input.GetAxis("PS4Triangle") == 1.0f)
+        {
+            ActUp.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            ActUp.GetComponent<Renderer>().material.color = Color.white;
+        }
     }
 
-    if (Input.GetAxis("PS4Circle") == 1.0f) {
-      ActRight.GetComponent<Renderer>().material.color = Color.red;
-    }
-    else {
-      ActRight.GetComponent<Renderer>().material.color = Color.white;
-    }
+    private void UpdateDirectionInputs()
+    {
+        if (Input.GetAxis("PS4LX") >= 1.0f || Input.GetAxis("PS4DX") >= 1.0f)
+        {
+            DirRight.GetComponent<Renderer>().material.color = Color.red;
+            DirLeft.GetComponent<Renderer>().material.color = Color.white;
+        }
+        else if (Input.GetAxis("PS4LX") <= -1.0f || Input.GetAxis("PS4DX") <= -1.0f)
+        {
+            DirRight.GetComponent<Renderer>().material.color = Color.white;
+            DirLeft.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            DirRight.GetComponent<Renderer>().material.color = Color.white;
+            DirLeft.GetComponent<Renderer>().material.color = Color.white;
+        }
 
-    if (Input.GetAxis("PS4Triangle") == 1.0f) {
-      ActUp.GetComponent<Renderer>().material.color = Color.red;
+        if (Input.GetAxis("PS4LY") >= 1.0f || Input.GetAxis("PS4DY") >= 1.0f)
+        {
+            DirUp.GetComponent<Renderer>().material.color = Color.red;
+            DirDown.GetComponent<Renderer>().material.color = Color.white;
+        }
+        else if (Input.GetAxis("PS4LY") <= -1.0f || Input.GetAxis("PS4DY") <= -1.0f)
+        {
+            DirUp.GetComponent<Renderer>().material.color = Color.white;
+            DirDown.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            DirUp.GetComponent<Renderer>().material.color = Color.white;
+            DirDown.GetComponent<Renderer>().material.color = Color.white;
+        }
     }
-    else {
-      ActUp.GetComponent<Renderer>().material.color = Color.white;
-    }
-  }
-
-  private void UpdateDirectionInputs() {
-    if (Input.GetAxis("PS4LX") >= 1.0f || Input.GetAxis("PS4DX") >= 1.0f) {
-      DirRight.GetComponent<Renderer>().material.color = Color.red;
-      DirLeft.GetComponent<Renderer>().material.color = Color.white;
-    }
-    else if (Input.GetAxis("PS4LX") <= -1.0f || Input.GetAxis("PS4DX") <= -1.0f) {
-      DirRight.GetComponent<Renderer>().material.color = Color.white;
-      DirLeft.GetComponent<Renderer>().material.color = Color.red;
-    }
-    else {
-      DirRight.GetComponent<Renderer>().material.color = Color.white;
-      DirLeft.GetComponent<Renderer>().material.color = Color.white;
-    }
-
-    if (Input.GetAxis("PS4LY") >= 1.0f || Input.GetAxis("PS4DY") >= 1.0f) {
-      DirUp.GetComponent<Renderer>().material.color = Color.red;
-      DirDown.GetComponent<Renderer>().material.color = Color.white;
-    }
-    else if (Input.GetAxis("PS4LY") <= -1.0f || Input.GetAxis("PS4DY") <= -1.0f) {
-      DirUp.GetComponent<Renderer>().material.color = Color.white;
-      DirDown.GetComponent<Renderer>().material.color = Color.red;
-    }
-    else {
-      DirUp.GetComponent<Renderer>().material.color = Color.white;
-      DirDown.GetComponent<Renderer>().material.color = Color.white;
-    }
-  }
 }
