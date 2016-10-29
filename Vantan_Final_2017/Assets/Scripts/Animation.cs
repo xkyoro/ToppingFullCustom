faed6f7d74d4bdd20cb89b5e4ad5f990fbd2adf9
xkyoro <min_n1202@yahoo.co.jp>
2016-10-29 12:21:47 +0900

    Update

--- Vantan_Final_2017/Assets/Scripts/Animation.cs
+++ Vantan_Final_2017/Assets/Scripts/Animation.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class Animation : MonoBehaviour
 {
-    
+
     [SerializeField]
     private GameObject DirUp = null;
     [SerializeField]
@@ -30,61 +30,57 @@ public class Animation : MonoBehaviour
 
     private int index = 0;
 
-    void Update()
+    [SerializeField]
+    private List<string> _Name;
+
+    public enum ButtonName
     {
-        if (check == null)
+        Square,
+        Circle,
+        Triangle,
+        Cross,
+        MaxButton,
+    }
+
+    void start()
+    {
+        check = new List<bool>();
+        checkButton = new List<bool>();
+
+        for (int i = 0; i < 100; i++)
         {
-            Debug.Log("null");
+            check.Add(false);
         }
 
-        if (check != null)
-        {
-            Debug.Log(check.Count);
-           
-            if (!check[index] && !checkButton[0] && PressCircle())
-            {
-                check[index] = true;
-                checkButton[0] = true;
-                if (index < check.Count - 1)
-                {
-                    index++;
-                }
-            }
-            if (!check[index] && !checkButton[1] && PressCross())
-            {
-                check[index] = true;
-                checkButton[1] = true;
-                if (index < check.Count - 1)
-                {
-                    index++;
-                }
-            }
-            if (!check[index] && !checkButton[2] && PressSpuare())
-            {
-                check[index] = true;
-                checkButton[2] = true;
-                if (index < check.Count - 1)
-                {
-                    index++;
-                }
-            }
-            if (!check[index] && !checkButton[3] && PressTriangle())
-            {
-                check[index] = true;
-                checkButton[3] = true;
-                if (index < check.Count - 1)
-                {
-                    index++;
-                }
-            }
 
-            if (checkButton[0] && checkButton[1] && checkButton[2] && checkButton[3])
+        for (int i = 0; i < (int)ButtonName.MaxButton; i++)
+        {
+            checkButton.Add(false);
+        }
+    }
+
+    public string getButtonName()
+    {
+        for (int i = 0; i < (int)ButtonName.MaxButton; i++)
+        {
+            if (isPressButton())
             {
-                for (int i = 0; i < 4; i++)
-                {
-                    checkButton[i] = false;
-                }
+                return _Name[i];
             }
+        }
+        return null;
+    }
+
+    void Update()
+    {
+        if (check.Count == 0)
+        {
+            check.Add(false);
+        }
+
+        if (check.Count != 0)
+        {
+
         }
 
         //if ()
@@ -93,47 +89,15 @@ public class Animation : MonoBehaviour
         }
     }
 
-    public void isNextIndex()
+    public bool isPressButton()
     {
-
-    }
-
-    public bool PressSpuare()
-    {
-        if (Input.GetAxis("PS4Square") == 1.0f)
+        for(int i = 0;i< (int)ButtonName.MaxButton; i++)
         {
-            Debug.Log("Square");
-            return true;
-        }
-        return false;
-    }
-
-    public bool PressCircle()
-    {
-        if (Input.GetAxis("PS4Circle") == 1.0f)
-        {
-            Debug.Log("Circle");
-            return true;
-        }
-        return false;
-    }
-
-    public bool PressTriangle()
-    {
-        if (Input.GetAxis("PS4Triangle") == 1.0f)
-        {
-            Debug.Log("Triangle");
-            return true;
-        }
-        return false;
-    }
-
-    public bool PressCross()
-    {
-        if (Input.GetAxis("PS4Cross") == 1.0f)
-        {
-            Debug.Log("cross");
-            return true;
+            if (Input.GetAxis(_Name[i]) == 1.0f)
+            {
+                Debug.Log(_Name[i]);
+                return true;
+            }
         }
         return false;
     }
